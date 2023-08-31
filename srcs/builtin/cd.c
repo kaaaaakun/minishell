16071b6c37f38741655b00d8ab72b0e069f5cc24
Hikari Hino <hhino@c1r10s8.42tokyo.jp>
2023-08-31 20:49:01 +0900

    pino0831_cd toka

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -1,11 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhino <hhino@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 18:15:57 by hhino             #+#    #+#             */
+/*   Updated: 2023/08/31 19:03:25 by hhino            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+void	ex_cd(t_info *status, t_stack *data)
+{
+	if (data->cmdlist->next == NULL)
+		//ホームに移動
+	else
+		
+	//t_stackからcmdの二つ目を読み込む
+	//cmd二つ目がアクセスが可能か確認
+		//bash: cd: ../1234: No such file or directory
+	//cmd二つ目が/だったら移動
+}
+
+
+// cd はHOMEに戻る
+// cd /は一番最初の階層に戻る
+// cd ../ 存在しないディレクトリ名　はcd ../のみ実行される
+// cd ../存在しないディレクトリ名　はエラー
+//d---------   3 hhino  2020  102 Aug 31 18:37 test/
+	//bash-3.2$ cd test/
+	//bash: cd: test/: Permission denied
+//dr--------   3 hhino  2020  102 Aug 31 18:37 test/
+	//bash-3.2$ cd test/
+	//bash: cd: test/: Permission denied
+//d-w-------   3 hhino  2020  102 Aug 31 18:37 test/
+	//bash-3.2$ cd test/
+	//bash: cd: test/: Permission denied
+//d--x------   3 hhino  2020  102 Aug 31 18:37 test/
+	//bash-3.2$ cd test/
+//bash-3.2$ cd ../ 1234 | touch a
+	//bash-3.2$ ls
+	//a          cd/        minishell/ test/
 
 void	ex_cd(char **split)
 {
 	(void)split;
 //	if (!split[1])
 //		$HOMEへ移動
-//	if (checkdir_access(split[1])指定のディレクトリにアクセスできるか判定)
+//	if (check_access(split[1])指定のディレクトリにアクセスできるか判定)
 //		エラーメッセージを出力
 //	そのフォルダに移動
 }
