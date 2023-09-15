32544a6613b3711d98c02189dc03c8e855a151bd
pino <hhino@student.42tokyo.jp>
2023-09-15 19:11:18 +0900

    unset dekinai

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: hhino <hhino@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 18:13:55 by hhino             #+#    #+#             */
-/*   Updated: 2023/09/14 19:52:00 by hhino            ###   ########.fr       */
+/*   Updated: 2023/09/15 16:48:14 by hhino            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 t_list	*search_envlist_for_unset(t_info *status, char *str)
 {
-	d_printf("[search_envlist_forunset]");
 	t_list	*env;
 	int		len;
 	char	*searched_word;
@@ -24,21 +23,13 @@ t_list	*search_envlist_for_unset(t_info *status, char *str)
 		return (NULL);
 	searched_word = ft_strjoin(str, "=");
 	len = ft_strlen(searched_word);
-	d_printf("[%s:%d]",searched_word,len);
 	while (env->next != NULL)
 	{
 		if (ft_strncmp(env->next->content, searched_word, len) == 0)
 			break ;
-		if (ft_strncmp(env->next->content, str, len) == 0)
+		else if (ft_strncmp(env->next->content, str, len) == 0)
 			break ;
 		env = env->next;
-	}
-	if (env->next == NULL) //修正する
-	{
-		if (ft_strncmp(env->content, searched_word, len) == 0)
-			return (env) ;
-		if (ft_strncmp(env->content, str, len) == 0)
-			return (env) ;
 	}
 	return (env);
 }
@@ -52,16 +43,29 @@ void	ex_unset(t_info *status, t_stack *data)
 	list = data->cmdlist->next;
 	while (list != NULL)
 	{
-		envlist = search_envlist_for_unset(status, ft_strdup(list->content));
-		if (envlist != NULL)
+		if (search_envlist_for_export(status, ft_strdup(list->content)))
 		{
-			ft_printf("\n\n[[[unset]]]%s\n", list->content);
-			temp = envlist;
-			envlist->next = envlist->next->next;
-			free(temp->content);
-			free(temp->next);
-			free(temp);
+			envlist = search_envlist_for_export(status, ft_strdup(list->content));
+			if (envlist->next == NULL)
+			{
+				ft_printf("\n\n[[[unset]]]%s\n", list->content);
+				temp = envlist;
+				free(temp->content);
+				free(temp->next);
+				// free(temp);
+				temp = NULL;
+			}
+			else if (envlist->next != NULL)
+			{
+				envlist = search_envlist_for_unset(status, ft_strdup(list->content));
+				ft_printf("\nprev;%s\nnextnext;%s", envlist->content, envlist->next->next->content);
+				temp = envlist->next;
+				envlist->next = envlist->next->next;
+				// free(temp->content);
+				// free(temp->next);
+				// free(temp);
+			}
+			list = list->next;
 		}
-		list = list->next;
 	}
 }
