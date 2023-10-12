33d3f9a03cc92355c0f74226cb04241b4e16767e
pino <hhino@c1r18s3.42tokyo.jp>
2023-10-12 19:39:25 +0900

    unset to export no leak wo naoshita
    ;

--- libft-puls/ft_calloc.c
+++ libft-puls/ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tokazaki <tokazaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hhino <hhino@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 19:30:56 by tokazaki          #+#    #+#             */
-/*   Updated: 2023/10/09 21:05:05 by tokazaki         ###   ########.fr       */
+/*   Updated: 2023/10/12 18:52:12 by hhino            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,10 @@ void	*malloc_ee(size_t size)
 	void		*result;
 
 	result = malloc(size);
-	//ft_printf("%p",result);
+	// ft_printf("%p:",result);
 	return (result);
 }
 //19:size or count がゼロの時には1byte分
 //24:size * count < SIZE_MAX になってしまう場合はNULLを返す
 //29:全てを'\0'で埋めて返す
+
