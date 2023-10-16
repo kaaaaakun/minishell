88fd57e703fa6bba4af5ca6942ba0ee2c51f1d5a
pino <hhino@c1r11s4.42tokyo.jp>
2023-10-16 15:25:45 +0900

    export no exit_status

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: hhino <hhino@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 18:14:07 by hhino             #+#    #+#             */
-/*   Updated: 2023/10/13 20:43:41 by hhino            ###   ########.fr       */
+/*   Updated: 2023/10/16 15:06:55 by hhino            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,7 @@ void	ex_export(t_info *status, t_stack *data)
 	flag = 0;
 	list = data->cmdlist;
 	if (list->next == NULL)
-		print_export_env(status->env);
+		print_export_env(status, status->env);
 	else
 	{
 		list = list->next;
@@ -96,64 +96,3 @@ void	ex_export(t_info *status, t_stack *data)
 	}
 	return ;
 }
-
-// void	ex_export(t_info *status, t_stack *data)
-// {
-// 	int		flag;
-// 	int		i;
-// 	t_list	*list;
-// 	char	*left;
-
-// 	flag = 0;
-// 	list = data->cmdlist;
-// 	if (list->next == NULL)
-// 		print_export_env(status->env);
-// 	else
-// 	{
-// 		list = list->next;
-// 		while (list != NULL)
-// 		{
-// 			flag = plus_equal_or_not(list->content);
-// 			if (valid_left(list->content, flag) == 0)
-// 			{
-// 				error_printf(" not a valid identifier\n");
-// 				status->exit_status = 1;
-// 			}
-// 			else
-// 			{
-// 				i = 0;
-// 				if (flag == 1)
-// 				{
-// 					while (list->content[i] != '+')
-// 						i++;
-// 				}
-// 				else
-// 				{
-// 					while (list->content[i] != '=' && list->content[i] != '\0')
-// 						i++;
-// 				}
-// 				left = ft_substr(list->content, 0, i);
-// 				if (search_envlist_for_export(status, left) != NULL)
-// 				{
-// 					if (flag == 1)
-// 						append_envlist(search_envlist_for_export(status, left),
-// 							ft_strdup(list->content));
-// 					else
-// 						overwrite_envlist(search_envlist_for_export(status,
-// 								left), ft_strdup(list->content));
-// 				}
-// 				else
-// 				{
-// 					if (flag == 1)
-// 						push_back(&status->env,
-// 							no_left_but_plus(ft_strdup(list->content)));
-// 					else
-// 						push_back(&status->env, ft_strdup(list->content));
-// 				}
-// 				free(left);
-// 			}
-// 			list = list->next;
-// 		}
-// 	}
-// 	return ;
-// }
